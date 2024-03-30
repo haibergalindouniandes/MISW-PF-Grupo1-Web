ddfe4ea36e4391426786a4f5b30e74cef0390962
CommitUser-at-407747150537 <android619@gmail.com>
2024-03-29 21:48:41 -0500

    Se actualiza formulario de registro de servicios y se implementa internacionalizacion base

--- cypress/e2e/register-service.spec.cy.ts
+++ cypress/e2e/register-service.spec.cy.ts
@@ -4,20 +4,20 @@ import { faker } from "@faker-js/faker";
 import { addDays, format } from 'date-fns';
 
 describe('Registration service e2e test', () => {
-  // it('Should validate the form fields so that they are not left empty', () => {
-  //   // Asignacion de credenciales
-  //   let email = 'administrador@sportapp.com'
-  //   let password = 'SportApp1234*'
-  //   // Ejecución de Login
-  //   const poSignin = new Signin();
-  //   poSignin.visit('/auth/signin')
-  //   poSignin.shouldSigninSuccess(email, password);
-  //   // Ejecución de la prueba de validación de errores del formulario del registro de servicio
-  //   let validationErrors = 7;
-  //   let poRegisterService = new Service();
-  //   poRegisterService.visit('/services/register');
-  //   poRegisterService.shouldValidateMessagesErrorInForm(validationErrors);
-  // })
+  it('Should validate the form fields so that they are not left empty', () => {
+    // Asignacion de credenciales
+    let email = 'administrador@sportapp.com'
+    let password = 'SportApp1234*'
+    // Ejecución de Login
+    const poSignin = new Signin();
+    poSignin.visit('/auth/signin')
+    poSignin.shouldSigninSuccess(email, password);
+    // Ejecución de la prueba de validación de errores del formulario del registro de servicio
+    let validationErrors = 7;
+    let poRegisterService = new Service();
+    poRegisterService.visit('/services/register');
+    poRegisterService.shouldValidateMessagesErrorInForm(validationErrors);
+  })
 
   it('Should allow you to register a service successfully', () => {
     // Asignacion de credenciales

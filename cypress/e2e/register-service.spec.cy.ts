5eedf1334b2a436bba939083248ae0ec3329cb15
CommitUser-at-407747150537 <android619@gmail.com>
2024-03-29 20:56:22 -0500

    Se actualiza formulario de registro de servicios y se implementa internacionalizacion base

--- cypress/e2e/register-service.spec.cy.ts
+++ cypress/e2e/register-service.spec.cy.ts
@@ -3,7 +3,22 @@ import { Signin } from "../page-objects/auth/signin";
 import { faker } from "@faker-js/faker";
 import { addDays, format } from 'date-fns';
 
-describe('Successful e2e test of registration service', () => {
+describe('e2e test of registration service', () => {
+  it('Should validate the form fields so that they are not left empty', () => {
+    // Asignacion de credenciales
+    let email = 'administrador@sportapp.com'
+    let password = 'SportApp1234*'
+    // Ejecución de Login
+    const poSignin = new Signin();
+    poSignin.visit('/auth/signin')
+    poSignin.shouldSigninSuccess(email, password);
+    // Ejecución de la prueba de validación de errores del formulario del registro de servicio
+    let validationErrors = 7;
+    let poRegisterService = new Service();
+    poRegisterService.visit('/services/register');
+    poRegisterService.shouldValidateMessagesErrorInForm(validationErrors);
+  })
+
   it('Should allow you to register a service successfully', () => {
     // Asignacion de credenciales
     let email = 'administrador@sportapp.com'
@@ -32,22 +47,5 @@ describe('Successful e2e test of registration service', () => {
     let poRegisterService = new Service();
     poRegisterService.visit('/services/register');
     poRegisterService.shouldSigninSuccess(serviceName, serviceDescription, serviceCost, servicePlace, serviceDate, serviceTime, serviceMinParticipants.toString(), serviceMaxParticipants.toString(), serviceFrecuency)
-  })
-});
-
-describe('Successful e2e test of registration service', () => {
-  it('Should allow you to register a service successfully', () => {
-    // Asignacion de credenciales
-    let email = 'administrador@sportapp.com'
-    let password = 'SportApp1234*'
-    // Ejecución de Login
-    const poSignin = new Signin();
-    poSignin.visit('/auth/signin')
-    poSignin.shouldSigninSuccess(email, password);
-    // Ejecución de la prueba de validación de errores del formulario del registro de servicio
-    let validationErrors = 7;
-    let poRegisterService = new Service();
-    poRegisterService.visit('/services/register');
-    poRegisterService.shouldValidateMessagesErrorInForm(validationErrors);
   })
 });

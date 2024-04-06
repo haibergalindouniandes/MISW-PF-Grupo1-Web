ee3322462e878dd7e454db1d81ed4902a7b66f3c
CommitUser-at-407747150537 <android619@gmail.com>
2024-04-05 23:46:53 -0500

    Se actualiza cloudbuild.yaml

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  
+
   e2e: {
     'baseUrl': 'http://localhost:4200'
   },
-  
-  
+
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
+    specPattern: '**/*.cy.ts',
+    defaultCommandTimeout:  15000
   }
-  
+
 })
--- run-tests.sh
+++ run-tests.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
-echo "<================== [Inicio] Ejecución de pruebas unitarias y cobertura ==================>"
+echo "<================== [Inicio] Configuración ==================>"
+TEST_PATH=tests-results
+mkdir -p $TEST_PATH
+echo "Se realiza la creación del directorio [$TEST_PATH]"
 MIN_COVERAGE=40
 echo "La cobertura minima establecida para pasar las pruebas es [$MIN_COVERAGE]"
-ng test --watch=false --browsers=ChromeHeadlessNoSandbox --code-coverage=true > coverage-results.txt
-COVERAGE=$(grep -oP 'Statements\s+:\s+\K\d+\.\d+' coverage-results.txt)
+COVERAGE_FILE=coverage-results.txt
+echo "El archivo con los resultados de las pruebas de cobertura es [$COVERAGE_FILE]"
+E2E_FILE=e2e-results.txt
+echo "El archivo con los resultados de las pruebas E2E es [$COVERAGE_FILE]"
+COVERAGE_PATH=coverage
+echo "El directorio con los resultados de las pruebas de cobertura es [$COVERAGE_PATH]"
+echo "<================== [Fin] Configuración ==================>"
+
+echo "<================== [Inicio] Ejecución de pruebas unitarias y cobertura ==================>"
+ng test --watch=false --browsers=ChromeHeadlessNoSandbox --code-coverage=true > $TEST_PATH/$COVERAGE_FILE
+COVERAGE=$(grep -oP 'Statements\s+:\s+\K\d+\.\d+' $TEST_PATH/$COVERAGE_FILE)
 echo "La cobertura total de las pruebas fue [$COVERAGE]"
 decimal=$(awk "BEGIN {print $COVERAGE}")
 if (( $(echo "$decimal < $MIN_COVERAGE" | bc -l) )); then
-  echo "La cobertura obtenida [$COVERAGE] es MENOR que la cobertura minima [$MIN_COVERAGE] requerida para pasar las pruebas"
+  echo "Error: La cobertura obtenida [$COVERAGE] es MENOR que la cobertura minima [$MIN_COVERAGE] requerida para pasar las pruebas"
+  echo "<================== [Fin][Error] Ejecución de pruebas unitarias y cobertura ==================>"
   exit 1
 fi
 echo "La cobertura obtenida [$COVERAGE] CUMPLE con los criterios para pasar las pruebas"
-echo "<================== [Fin] Ejecución de pruebas unitarias y cobertura ==================>"
+mv $COVERAGE_PATH $TEST_PATH/
+echo "Se mueve directorio [$COVERAGE_PATH] a [$TEST_PATH/$COVERAGE_PATH]"
+echo "<================== [Fin][Exitoso] Ejecución de pruebas unitarias y cobertura ==================>"
 
 echo "<================== [Inicio] Levantamiento del proyecto Angular ==================>"
 nohup ng serve &
 sleep 20
 if ! curl -s localhost:4200; then
-  echo "Error: ng serve no está escuchando en localhost:4200"
+  echo "Error: NG serve no está escuchando en localhost:4200"
+  echo "<================== [Fin][Error] Levantamiento del proyecto Angular ==================>"
   exit 1
 fi
-echo "<================== [Fin] Levantamiento del proyecto Angular ==================>"
+echo "<================== [Fin][Exitoso] Levantamiento del proyecto Angular ==================>"
 
 echo "<================== [Inicio] Ejecución de pruebas e2e ==================>"
-npx cypress run --headless
-echo "<================== [Fin] Ejecución de pruebas e2e  ==================>"
+npx cypress run --headless > $TEST_PATH/$E2E_FILE
+if grep -q "AssertionError" $TEST_PATH/$E2E_FILE; then
+  echo "Error: Se presetaron fallas en la ejecución de las pruebas e2e"
+  echo "<================== [Fin][Error] Ejecución de pruebas e2e  ==================>"
+  exit 1
+fi
+echo "<================== [Fin][Exitoso] Ejecución de pruebas e2e  ==================>"

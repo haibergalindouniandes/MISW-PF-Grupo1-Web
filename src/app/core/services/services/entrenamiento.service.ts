c1bc8416111c5f62c8a352beafb26cc137b5f4f5
jhon.caicedo <jhon.caicedo@rappi.com>
2024-04-24 23:47:19 -0500

    se agrega funcionalidad de home

--- src/app/core/services/services/entrenamiento.service.ts
+++ src/app/core/services/services/entrenamiento.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-import { Alimentacion } from '../../models/services/alimentacion';
 import { Entrenamiento } from '../../models/services/entrenamiento';
 
 @Injectable({
--- src/app/features/home/components/card/card.component.ts
+++ src/app/features/home/components/card/card.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { ConsultarPlanAlimentacionService } from '../../../../core/services/services/alimentacion.service';
+import { ConsultarPlanEntrenamientoService } from '../../../../core/services/services/entrenamiento.service';
 import { Validators } from '@angular/forms';
 import { Alimentacion, PlanAlimentacion } from '../../../../core/models/services/alimentacion';
+import { PlanEntrenamiento } from '../../../../core/models/services/entrenamiento';
 
 
 @Component({
@@ -14,36 +16,55 @@ import { Alimentacion, PlanAlimentacion } from '../../../../core/models/services
   styleUrl: './card.component.scss'
 })
 export class CardComponent implements OnInit {
+  datosEntrenamiento: PlanEntrenamiento | undefined;
+  datosAlimentacion: PlanAlimentacion | undefined;
 
   constructor(
-    private alimentacionService: ConsultarPlanAlimentacionService,
+    private entrenamientoService: ConsultarPlanEntrenamientoService,
+    private alimentacionService: ConsultarPlanAlimentacionService
   ) { }
 
-  columnas: string[] = ['Día','Alimentación'];
-  datos : PlanAlimentacion | undefined;
+  columnas: string[] = ['Día','Entrenamiento','Alimentación'];
   objectKeys = Object.keys;
   dia:any[]=[];
   planes: any[][]= [];
 
   ngOnInit() {
-    console.log('Consultando servicio plan alimentacion')
-    this.alimentacionService.consulrarPlanAlimentacion().subscribe(success => {
-      console.log('resultado:::::::::::')
-      this.datos = success.plan_alimentacion;
-      console.log(success.plan_alimentacion);
-      console.log(this.datos.id);
-      this.planes= [
-        ['Lunes',this.datos.lunes],
-        ['Martes',this.datos.martes],
-        ['Miercoles',this.datos.miercoles],
-        ['Jueves',this.datos.jueves],
-        ['Viernes',this.datos.viernes],
-        ['Sabado',this.datos.sabado],
-        ['Domingo',this.datos.domingo]
-      ]
-      console.log('plan alimentacion:::::::::::')
-      console.log(this.planes);
-    })   
+    console.log('Consultando servicio plan entrenamiento')
+    this.entrenamientoService.consulrarPlanAEntrenamiento().subscribe((exitoso: { plan_entrenamiento: PlanEntrenamiento; }) => {
+      console.log('resultado:::::::::::');
+      this.datosEntrenamiento = exitoso.plan_entrenamiento;
+      console.log(exitoso.plan_entrenamiento);
+      console.log(this.datosEntrenamiento.id);
+      console.log('plan Entrenamiento:::::::::::');
+
+      console.log('Consultando servicio plan alimentacion')
+      this.alimentacionService.consulrarPlanAlimentacion().subscribe(success => {
+        console.log('resultado:::::::::::');
+        this.datosAlimentacion = success.plan_alimentacion;
+        console.log(success.plan_alimentacion);
+        console.log(this.datosAlimentacion.id);
+        this.planes= [
+          ['Lunes', this.datosEntrenamiento?.lunes,this.datosAlimentacion?.lunes],
+          ['Martes',this.datosEntrenamiento?.martes,this.datosAlimentacion?.martes],
+          ['Miercoles',this.datosEntrenamiento?.miercoles,this.datosAlimentacion?.miercoles],
+          ['Jueves',this.datosEntrenamiento?.jueves,this.datosAlimentacion?.jueves],
+          ['Viernes',this.datosEntrenamiento?.viernes,this.datosAlimentacion?.viernes],
+          ['Sabado',this.datosEntrenamiento?.sabado,this.datosAlimentacion?.sabado],
+          ['Domingo',this.datosEntrenamiento?.domingo,this.datosAlimentacion?.domingo]
+        ]
+        
+        console.log('plan alimentacion:::::::::::');
+        console.log(this.planes);
+      });   
+
+
+
+    });
+
+
+   
+
     
   }
 }
